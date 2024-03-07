86244c96f425ff00282bab4c8052032bf78116a1
mainaerick <niteric@gmail.com>
2024-03-07 13:02:49 +0300

    feat: add read and write message channels

--- server/internal/ws/hub.go
+++ server/internal/ws/hub.go
@@ -21,3 +21,43 @@ func NewHub() *Hub {
 		Broadcast:  make(chan *Message, 5),
 	}
 }
+
+func (h *Hub) Run() {
+	for {
+		select {
+		// cl is channel
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				r := h.Rooms[cl.RoomID]
+
+				if _, ok := r.Clients[cl.ID]; !ok {
+					r.Clients[cl.ID] = cl
+				}
+			}
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				if _, ok := h.Rooms[cl.RoomID].Clients[cl.ID]; ok {
+					// broadcast client has left the room
+					if len(h.Rooms[cl.RoomID].Clients) != 0 {
+						h.Broadcast <- &Message{
+							Content:  "user left the chat",
+							RoomID:   cl.RoomID,
+							Username: cl.Username,
+						}
+					}
+
+					delete(h.Rooms[cl.RoomID].Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
+
+		case m := <-h.Broadcast:
+			if _, ok := h.Rooms[m.RoomID]; ok {
+
+				for _, cl := range h.Rooms[m.RoomID].Clients {
+					cl.Message <- m
+				}
+			}
+		}
+	}
+}
