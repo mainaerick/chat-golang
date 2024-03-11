04849c24cb18bddbe6b0a9bb8653180bdb7be569
mainaerick <niteric@gmail.com>
2024-03-11 14:42:19 +0300

    chore: add cors

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import AuthContextProvider from "../modules/auth_provider";
+import WebSocketProvider from "../modules/websocket_provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  <div>
-  <Component {...pageProps} /></div></>
+  <AuthContextProvider>
+        <WebSocketProvider>
+  <div className='flex flex-col md:flex-row h-full min-h-screen font-sans'>
+  <Component {...pageProps} /></div></WebSocketProvider></AuthContextProvider></>
 }
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import { API_URL } from '../../constants'
+import { AuthContext, UserInfo } from '../../modules/auth_provider'
+import router from 'next/router'
+import React, { useContext, useEffect, useState } from 'react'
 
 const index = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { authenticated } = useContext(AuthContext)
+  useEffect(() => {
+    if (authenticated) {
+      router.push('/')
+      return
+    }
+  }, [authenticated])
+
+  const submitHandler = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    try {
+      const res = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      })
+
+      const data = await res.json()
+      if (res.ok) {
+        const user: UserInfo = {
+          username: data.username,
+          id: data.id,
+        }
+
+        localStorage.setItem('user_info', JSON.stringify(user))
+        return router.push('/')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div className='flex items-center justify-center min-w-full min-h-screen'>
       <form className='flex flex-col md:w-1/5'>
@@ -10,20 +47,20 @@ const index = () => {
         <input
           placeholder='email'
           className='p-3 mt-8 rounded-md border-2 border-grey focus:outline-none focus:border-blue'
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type='password'
           placeholder='password'
           className='p-3 mt-4 rounded-md border-2 border-grey focus:outline-none focus:border-blue'
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button
           className='p-3 mt-6 rounded-md bg-blue font-bold text-white'
           type='submit'
-          // onClick={submitHandler}
+          onClick={submitHandler}
         >
           login
         </button>
